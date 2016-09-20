4cb1a4ce4c644ae65246b758056728f139101827
justinligraph <justin.li@graphsql.com>
2016-09-19 17:32:23 -0700

    kwargs add example using dict.

--- kwargs_example.py
+++ kwargs_example.py
@@ -1,8 +1,14 @@
 
 def foo(**kwargs):
   for key, value in kwargs.iteritems():
-    print key, ':', value
-  print type(kwargs)
+    print '\t', key, ':', value
+  print '\tType of kwargs is:' , type(kwargs)
 
 if __name__ == '__main__':
+  print '**Use named parameter**'
   foo(first_name="Justin", last_name="Li")
+
+  print
+  print '**Use dict**'
+  params = {'first_name': 'Justin', 'last_name': 'Li'}
+  foo(**params)
